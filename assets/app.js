dd81ebf732d23185fcec6ef87ceac6514acb321d
JMurphy <murph_iraq@yahoo.com>
2018-08-15 18:19:40 -0400

    initialized firebase

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,16 @@
 console.log("connected!");
+
+//Initializing Firebase
+
+var config = {
+    apiKey: "AIzaSyDFMHeTXeTrFYaCe_ONw7tUm3_5y98dqYA",
+    authDomain: "train-scheduler-uncc.firebaseapp.com",
+    databaseURL: "https://train-scheduler-uncc.firebaseio.com",
+    projectId: "train-scheduler-uncc",
+    storageBucket: "train-scheduler-uncc.appspot.com",
+    messagingSenderId: "1042163805875"
+  };
+  firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
