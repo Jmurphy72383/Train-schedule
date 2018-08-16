9f121e95d501a829a197af74a44388d63b386b16
JMurphy <murph_iraq@yahoo.com>
2018-08-15 20:48:19 -0400

    added background image, finally got moment.js to work

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,42 @@ var config = {
 
 // Create a variable to reference the database
 var database = firebase.database();
+var trainName = "";
+var destination = "";
+var firstTrainTime = 0;
+var frequency = 0;
+
+$("button").on("click", function() {
+    event.preventDefault();
+    
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrainTime = $("#firstTrainTime").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#firstTrainTime").val("");
+    $("#frequency").val("");
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    })
+})
+
+database.ref().on("child_added", function(snapshot) {
+
+    var sv = snapshot.val();
+    var firstTimeConverted = moment(snapshot.val().firstTrainTime, "hh:mm").subtract(1, "days");
+    var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+    var remainder = timeDiff % snapshot.val().frequency;
+    var minsUntilTrain = snapshot.val().frequency - remainder;
+    var nextTrainTime = moment().add(minsUntilTrain, "minutes");
+
+    $("tbody").append("<tr><td>" + sv.trainName + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + moment(nextTrainTime).format('hh:mm') + "</td><td>" + minsUntilTrain + "</td></tr>");
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
